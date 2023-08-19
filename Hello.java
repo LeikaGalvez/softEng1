478fb80475f5cf3097212c998c5af2070d99a6d6
LeikaGalvez <142652629+LeikaGalvez@users.noreply.github.com>
2023-08-19 14:47:14 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+  public static void main(String[] args){
+    System.out.println("Hello, Software Engineering course!");
+    System.out.println("My name is GALVEZ, Leika Anne P.!);
+  }
+}
