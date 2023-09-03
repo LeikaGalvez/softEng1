f7dbd125d7f448ab551eaaac3c8ad7751a1d3c5b
Leika <leika.galvez@gmail.com>
2023-09-03 18:23:11 +0800

    added a new file Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering course!");
     System.out.println("My name is GALVEZ, Leika Anne P.!);
+    System.out.println ("Complicated world of SoftDev that is why it pays much!");
+
   }
 }
